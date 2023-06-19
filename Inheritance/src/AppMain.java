f4886439f185832469a5142013f6b6f308d229fa
okyws <okywahyu1@gmail.com>
2023-06-20 01:14:47 +0700

    InheritanceV4 (Super Constructor)

--- Inheritance/src/AppMain.java
+++ Inheritance/src/AppMain.java
@@ -9,21 +9,27 @@ public class AppMain {
     teacher1.address = "Belitung";
     teacher1.subject = "IPA";
 
-    Doctor doctor1 = new Doctor();
-    doctor1.name = "Aji";
-    doctor1.address = "Solo";
-    doctor1.specialist = "Dentis";
-
     Programmer programmer1 = new Programmer();
     programmer1.name = "Andika";
     programmer1.address = "Jakarta";
     programmer1.technology = "JAVA";
 
+    // object Doctor menggunakan constructor default
+    Doctor doctor1 = new Doctor();
+    doctor1.name = "Aji";
+    doctor1.address = "Solo";
+    doctor1.specialist = "Dentis";
+
+    // object Doctor menggunakan constructor berparameter
+    Doctor doctor2 = new Doctor("Danu", "Tangerang", "Cardiologist");
+    doctor2.greeting();
+    System.out.println();
+
+    doctor1.greeting();
+    System.out.println();
     person1.greeting();
     System.out.println();
     teacher1.greeting();
-    System.out.println();
-    doctor1.greeting();
     System.out.println();
     programmer1.greeting();
     System.out.println();
--- Inheritance/src/Doctor.java
+++ Inheritance/src/Doctor.java
@@ -2,10 +2,23 @@ public class Doctor extends Person {
   String specialist;
 
   public Doctor() {
-
+    /*
+     * meskipun kita kita menuliskan " super(); " tetapi java sudah menyediakannya
+     * secara default meski kita tidak memanggilnya
+     */
   }
 
   public Doctor(String name, String address, String specialist) {
+    /*
+     * short code:
+     * super(name, address);
+     *
+     * alternatif cara penggunaan super constructor
+     * super();
+     * super.name = name;
+     * super.address = address;
+     */
+
     super(name, address);
     this.specialist = specialist;
   }
