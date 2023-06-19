10a41319611fb653c12b45b7ee5d2257e0dde040
okyws <okywahyu1@gmail.com>
2023-06-20 03:00:17 +0700

    PolymorphismV2 (Type Casts and Check)

--- Polymorphism/src/AppMain.java
+++ Polymorphism/src/AppMain.java
@@ -1,7 +1,5 @@
 public class AppMain {
   public static void main(String[] args) {
-    Person person1 = new Programmer("Oky", "Bandung", "Java");
-    person1.greeting();
 
     /*
      * Implementasi Polymorphism
@@ -13,6 +11,33 @@ public class AppMain {
      * System.out.println(((Programmer) person1).technology);
      *
      */
+
+    Person person1 = new Programmer("Oky", "Bandung", "Java");
+    // person1.greeting();
     System.out.println(((Programmer) person1).technology);
+
+    Person person2 = new Teacher("Rizki", "Bogor", "Matematika");
+    Person person3 = new Doctor("Ahmad", "Solo", "Pedistrician");
+
+    sayHello(person1);
+    sayHello(person2);
+    sayHello(person3);
+  }
+
+  static void sayHello(Person person) {
+    String message;
+    if (person instanceof Programmer) {
+      Programmer programmer = (Programmer) person;
+      message = "Hello, " + programmer.name + ". Seorang Programmer " + programmer.technology + ".";
+    } else if (person instanceof Teacher) {
+      Teacher teacher = (Teacher) person;
+      message = "Hello, " + teacher.name + ". Seorang Guru " + teacher.subject + ".";
+    } else if (person instanceof Doctor) {
+      Doctor doctor = (Doctor) person;
+      message = "Hello, " + doctor.name + ". Seorang Dokter " + doctor.specialist + ".";
+    } else {
+      message = "Hello, " + person.name + ".";
+    }
+    System.out.println(message);
   }
 }
