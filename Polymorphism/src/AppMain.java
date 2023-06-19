2e01a4bc82ab53509c542040b0e3d5c503a21fcc
okyws <okywahyu1@gmail.com>
2023-06-20 02:44:50 +0700

    PolymorphismV1

--- Polymorphism/src/AppMain.java
+++ Polymorphism/src/AppMain.java
@@ -2,6 +2,17 @@ public class AppMain {
   public static void main(String[] args) {
     Person person1 = new Programmer("Oky", "Bandung", "Java");
     person1.greeting();
-    System.out.println();
+
+    /*
+     * Implementasi Polymorphism
+     *
+     * koding berikut tidak akan bisa berjalan untuk mengakses technology
+     * System.out.println(person1.technology);
+     *
+     * solusinya
+     * System.out.println(((Programmer) person1).technology);
+     *
+     */
+    System.out.println(((Programmer) person1).technology);
   }
 }
