fcbca3d880e10d55af3d2eb21203d5f711b37a23
okyws <okywahyu1@gmail.com>
2023-06-19 23:00:10 +0700

    Variable Shadowing

--- src/Person.java
+++ src/Person.java
@@ -14,10 +14,17 @@ public class Person {
     name = paramName;
   }
 
-  // Constructor With Parameter
-  Person(String paramName, String paramAddress) {
-    this(paramName);
-    address = paramAddress;
+  // Variable Shadowing
+  Person(String name, String address) {
+    // penamaan field dan parameter yang sama mengakibatkan terjadinya variable
+    // shadowing
+    // field nama tidak akan berubah name = name;
+    // field address tidak akan berubah address = address;
+
+    // cara mengatasinya dengan memakai keyword this
+    // this.name menunjukan field dari kelas itu sendiri
+    this.name = name;
+    this.address = address;
   }
 
   // method with value
